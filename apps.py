55724fc6b00456646ffd7459fb90f18d64cb2457
FLAME-CYBER-404 <102447853+FLAME-CYBER-404@users.noreply.github.com>
2024-10-28 19:20:55 +0600

    Update apps.py

--- apps.py
+++ apps.py
@@ -11,7 +11,7 @@ with st.sidebar:
     st.write('This chatbot is created using the open-source Llama 2 LLM model from Meta.')
     if 'REPLICATE_API_TOKEN' in st.secrets:
         st.success('API key already provided!', icon='✅')
-        replicate_api = st.secrets['REPLICATE_API_TOKEN']
+        replicate_api = st.secrets['r8_OzmW4uh2yUIMFlgxd6ybzldCeZtl6w33AOmM6']
     else:
         replicate_api = st.text_input('Enter Replicate API token:', type='password')
         if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
